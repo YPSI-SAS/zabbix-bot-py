55f14dfdb98be8964d820bc06bfe7c1e098d793f
Mélissa BERTIN <melissa.bertin@ypsi.fr>
2022-06-01 12:19:45 +0200

    add SLA informations into service report

--- report_service.py
+++ report_service.py
@@ -2,9 +2,9 @@
 from reportlab.lib import colors
 from reportlab.platypus import SimpleDocTemplate, Spacer, Image, Table, TableStyle
 from reportlab.lib.units import inch, cm
-from action import get_time, get_image_data
-
+from action import get_date_value_depending_period, get_val_time
 from report import Report
+import numpy as np
 
 
 class ReportService(Report):
@@ -40,7 +40,7 @@ class ReportService(Report):
         for tag in self.information_service['problem_tags']:
             tag_val = []
             tag_val.append(tag['tag'])
-            tag_val.append(self.convert_operator_to_text(tag['operator']))
+            tag_val.append(self.convert_operator_to_text(tag['operator'], _))
             tag_val.append(tag['value'])
             data_tag.append(tag_val)
         table = Table(data_tag, hAlign='LEFT')
@@ -53,6 +53,64 @@ class ReportService(Report):
             ('INNERGRID', (0, 0), (-1, -1), 0.5, colors.black),
         ]))
         self.story.append(table)
+
+    def write_sla_information(self,part,_):
+        """Write SLA information of service"""
+        sla_service = self.api.get_sla_by_service(service_id=self.information_service['serviceid'])
+        self.write_paragraph(
+            part+". "+_("SLA"), 'Heading1Set')
+
+        if len(sla_service) != 0:    
+            for sla in sla_service:
+                index = sla_service.index(sla)+1
+                sla_report = self.api.get_sla_report_by_service(sla_id=sla['slaid'], service_id=self.information_service['serviceid'])
+                sla_report_period = sla_report['periods']
+                sla_report_period.reverse()
+                sla_report_sli = list()
+                for i in range(len(sla_report_period)):
+                    sla_report_sli.append(sla_report['sli'][i][0])
+                sla_report_sli.reverse()
+
+                self.write_paragraph(
+                    part+"."+str(index)+" " +sla['name'], 'Heading2Set')
+
+                self.story.append(Spacer(1, 12))
+                data_sla = [[self.get_column_name(sla['period'], _), _('SLO'), _('SLI'), _('Uptime'), _('Downtime'), _('Error budget')]]
+
+                for i in range(len(sla_report_period)):
+                    sla_val = []
+                    sla_val.append(get_date_value_depending_period(sla['period'], sla_report_period[i]['period_from'], sla_report_period[i]['period_to']))
+                    sla_val.append(sla['slo'])
+                    sla_val.append(float(np.round(sla_report_sli[i]['sli'], 4)))
+                    sla_val.append(get_val_time(sla_report_sli[i]['uptime']))
+                    sla_val.append(get_val_time(sla_report_sli[i]['downtime']))
+                    error_budget = get_val_time(abs(sla_report_sli[i]['error_budget']))
+                    if sla_report_sli[i]['error_budget'] < 0:
+                        error_budget = "-" + error_budget
+                    sla_val.append(error_budget)
+                    data_sla.append(sla_val)
+
+                table = Table(data_sla, hAlign='LEFT')
+                table.setStyle(TableStyle([
+                    ('FONT', (0, 0), (-1, -1), 'Helvetica'),
+                    ('FONT', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                    ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+                    ('BOX', (0, 0), (-1, -1), 0.5, colors.black),
+                    ('INNERGRID', (0, 0), (-1, -1), 0.5, colors.black),
+                ]))
+                self.story.append(table)
+
+    def get_column_name(self,period, _):
+        """Convert period to text"""
+        switcher = {
+            "0": _("Day"),
+            "1": _("Week"),
+            "2": _("Month"),
+            "3": _("Quarter"),
+            "4": _("Year")
+        }
+        return switcher.get(period, "invalid status")
 
     def write_parents_information(self, part, _):
         """Write parents information of service"""
@@ -115,11 +173,12 @@ class ReportService(Report):
         self.write_paragraph(
             self._("Service")+" : "+self.information_service['name'], 'TitleSet')
         self.write_general_information("1", self._)
-        self.write_parents_information("2", self._)
-        self.write_children_information("3", self._)
+        self.write_sla_information("2", self._)
+        self.write_parents_information("3", self._)
+        self.write_children_information("4", self._)
         self.write_problems_information(
-            "4", self._, self.information_service['problem_events'])
-        self.write_host_information("5", self._, self.host_ids)
-        self.write_trigger_information("6", self._, self.trigger_ids)
+            "5", self._, self.information_service['problem_events'])
+        self.write_host_information("6", self._, self.host_ids)
+        self.write_trigger_information("7", self._, self.trigger_ids)
         doc.build(self.story)
         return name_file, self.list_images
